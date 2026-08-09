83f2a26f71a5e589973f3082d0ade02a364e9ad7
Rahul Pardhi <pardhirahul2004@gmail.com>
2026-08-09 12:33:11 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 12355;
+let accountName ="rahul"
+var accountPassword = "1234"
+accoundCity = "nagpur"
+let accountState;
+
+//accountId = 2 // not allowed
+
+accountEmail = "rahul@gmail.com"
+accountPassword = "123344"
+accountCity = "gondia"
+
+console.log("accountId");
+
+/*
+prefer not to use var bacause of issue in block scope and functional scope */
+
+
+console.table([accountEmail, accountPassword, accountCity, accountState, accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as a never version
+
+  //alert (3+3) // we are using node.js , not browser
+
+ //   console.log(3
+ // +
+ // 3) // code readability should be high
+
+    console.log("Rahul")
+
+    let name = "Rahul" // string data type 
+
+    // number =>  2 to power 53 
+    // bigint
+    //string =>""
+    // boolean => true/false
+    // null => standa lone value
+    // undefined => 
+    // symbol => unique
+
+    // object 
+
+    console.log(typeof "Rahul");// string
+    console.log(typeof undefined) // undefined
+    console.log(typeof null) // object
