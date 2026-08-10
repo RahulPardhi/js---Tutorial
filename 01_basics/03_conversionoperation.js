b7ce7e33467052819b96b1af93d76dfee5e99669
Rahul Pardhi <pardhirahul2004@gmail.com>
2026-08-10 19:20:56 +0000

    comparision

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -1,11 +1,11 @@
 let score = 33
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log( valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN
@@ -23,5 +23,44 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log( typeof stringNumber);
+// console.log(stringNumber);
+// console.log( typeof stringNumber);
+
+// *************** Operations ***************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**2);
+//console.log(2/2);
+//console.log(2%2);
+
+let str1 = "hello"
+let str2 =  " Rahul"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+
+// console.log("1" + 2); // 12
+// console.log(1 + "2"); // 12
+// console.log("1" + 2 + 2); // 122
+// console.log(1 + 2 + "2"); // 32
+
+// console.log(3 + 4) * 5 % 2); // this is not good way to programming
+
+// console.log(+true); // 1
+// console.log(+"");
+
+let num1, num2, num3 
+
+num1 + num2 + num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter); // 101
+
