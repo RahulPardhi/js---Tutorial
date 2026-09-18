b34d88ee0552c1899dd87089d49e33443aa78a57
Rahul Pardhi <pardhirahul2004@gmail.com>
2026-09-18 11:26:05 +0000

    functions

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -9,7 +9,26 @@ function sayMyName(){
 
 // sayMyName()
 
-function addTwoNumbers(number1 + number2){
-    console.log(number1 + number2);
+function addTwoNumbers(number1, number2){
+  // let result = number1 + number2
+  // return result
+  return number1 + number2
 }
-addTwoNumbers(3, 4)
+ const result = addTwoNumbers(3, 70)
+ // console.log("Result:", result);
+
+
+ function loginUserMessage(username){
+  return `${username} just logged in`
+ }
+ // console.log(loginUserMessage("Rahul"))
+
+ function loginUserMessage(username = "rahul"){
+  if(!username){
+    console.log("please enter your username");
+    return
+  }
+  return `${usename} just logged in`
+ }
+
+ console.log(loginUserMessage())
